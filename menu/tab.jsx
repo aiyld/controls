b9a0946a4ed6f73e03d6fbe722bebb32641915c7
YangLidong <aiyld@hotmail.com>
2018-04-18 23:58:37 +0800

    1. 组件增加新特性

--- menu/tab.jsx
+++ menu/tab.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {PropTypes} from "prop-types";
 import "./tab.less";
 
@@ -7,79 +7,91 @@ import "./tab.less";
  *
  */
 export default class Tab extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            selectIndex: -1,
+            selectIndex: -1
         };
-        this.onSelectChangeHandler.bind(this);
+        this
+            .onSelectChangeHandler
+            .bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setState({selectIndex: this.props.index});
     }
 
-    onSelectChangeHandler(item, i){
+    onSelectChangeHandler(item, i) {
         let {onSelectChange} = this.props;
-        if(onSelectChange){
+        if (onSelectChange) {
             onSelectChange(item, i);
         }
 
         this.setState({selectIndex: i});
     }
 
-    render(){
+    render() {
         let {items, index} = this.props;
         let {selectIndex} = this.state;
 
         items = items || [];
 
-        return <ul className="ld ld-tab flexshow">
-            {
-                items.map((val, i)=>{
-                    let message, className="", curIndex, curClass="";
-                    if(typeof val == "string"){
-                        message = val;
-                    }if(typeof val == "object"){
-                        message = val.title;
-                        className = val.className;
-                    }
+        let className = this.props.ver
+            ? "ver"
+            : "";
 
-                    if(selectIndex == -1){
-                        index = index || 0;
-                        if(index == i){
-                            curClass = "active";
-                        }
-                    }else if(selectIndex == i){
+        return <ul
+            className={"ld ld-tab flexshow " + (this.props.className || "") + className}>
+            {items.map((val, i) => {
+                let message,
+                    className = "",
+                    curIndex,
+                    curClass = "";
+                if (typeof val == "string") {
+                    message = val;
+                }
+                if (typeof val == "object") {
+                    message = val.title;
+                    className = val.className;
+                }
+
+                if (selectIndex == -1) {
+                    index = index || 0;
+                    if (index == i) {
                         curClass = "active";
                     }
+                } else if (selectIndex == i) {
+                    curClass = "active";
+                }
 
-                    return <li key={i} className={curClass}
-                               onClick={this.onSelectChangeHandler.bind(this, val, i)}>
-                        <a>
-                            <div className={"iconfont "+className}><span className="item-msg">{message}</span></div>
-                        </a>
-                    </li>
-                })
-            }
+                return <li
+                    key={i}
+                    className={curClass}
+                    onClick={this
+                    .onSelectChangeHandler
+                    .bind(this, val, i)}>
+                    <a>
+                        <div className={"iconfont " + className}>
+                            <span className="item-msg">{message}</span>
+                        </div>
+                    </a>
+                </li>
+            })
+}
         </ul>
     }
 }
 
 Tab.propTypes = {
-    items: PropTypes.array,                        //菜单项 menu items
-                                                   //               [
-                                                   //                   "",
-                                                   //                   {
-                                                   //                       src: "", string or Object,
-                                                   //                   }
-                                                   //               ]
-    index: PropTypes.number,                       //默认选中的索引 The index of item, which is selcted by default
-    onSelectChange: PropTypes.func,                //选择更改事件 Selection change event
+    items: PropTypes.array, //菜单项 menu items
+    //               [                   "",                   {
+    //   src: "", string or Object,                   }               ]
+    index: PropTypes.number, //默认选中的索引 The index of item, which is selcted by default
+    onSelectChange: PropTypes.func, //选择更改事件 Selection change event
 }
 
 Tab.defaultProps = {
     items: [],
     index: 0,
-    onSelectChange: null,
+    onSelectChange: null
 }
